060a3fe43ba2edaf1df2a0598acac6fbbd383ad5
Avokaci <if19b123@technikum-wien.at>
2020-10-17 21:51:35 +0200

    Cards finalized and properties set, introduced differenct Spellcards, started with battle logic

--- MonsterTradingCardGame.Lib/Game.cs
+++ MonsterTradingCardGame.Lib/Game.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MonsterTradingCardGame.Lib.Monsters;
+using MonsterTradingCardGame.Lib.Spells;
+using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 
 namespace MonsterTradingCardGame.Lib
@@ -7,14 +10,37 @@ namespace MonsterTradingCardGame.Lib
     public class Game
     {
         bool attackerToken;
-        public void battle()
+        public void battle(Card ownCard, Card enemyCard)
         {
+            //Special Rules
+            //Goblin vs Dragon
+            if((ownCard is Goblin && enemyCard is Dragon) || (enemyCard is Goblin && ownCard is Dragon))
+            {
+                //dragon win 
+            }
+            //Wizzard vs Ork
+            if((ownCard is Wizzard && enemyCard is Ork)||(enemyCard is Wizzard && ownCard is Ork) )
+            { 
+                //wizard win
+            }
+            //Knight vs Waterspell
+            if((ownCard is Knight && enemyCard is WaterSpell) || (enemyCard is Knight && ownCard is WaterSpell))
+            {
+                //waterspell win
+            }
+            //Kraken vs Spell
+            if((ownCard is Kraken && enemyCard is SpellCard) ||(enemyCard is Kraken && ownCard is SpellCard))
+            {
+                //kraken win
+            }
+            //FireElve vs Dragon
+            if((ownCard is FireElve && enemyCard is Dragon) || (enemyCard is FireElve && ownCard is Dragon))
+            {
+                //Fire Elve win
+            }
 
         }
-        public void checkEffectiveness()
-        {
-
-        }
+        
 
     }
 }
